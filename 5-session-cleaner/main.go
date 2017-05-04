589f5baf9cc47b6617487ca39cf7c044b9a8b431
ihordiachenko <i.diachenko@student.csn.khai.edu>
2017-05-04 18:33:14 +0300

    Finished task #5

--- 5-session-cleaner/main.go
+++ 5-session-cleaner/main.go
@@ -20,24 +20,31 @@ package main
 import (
 	"errors"
 	"log"
+	"sync"
+	"time"
 )
 
 // SessionManager keeps track of all sessions from creation, updating
 // to destroying.
 type SessionManager struct {
 	sessions map[string]Session
+	mutex    sync.Mutex
 }
 
 // Session stores the session's data
 type Session struct {
-	Data map[string]interface{}
+	Data      map[string]interface{}
+	UpdatedAt time.Time
 }
 
 // NewSessionManager creates a new sessionManager
 func NewSessionManager() *SessionManager {
 	m := &SessionManager{
 		sessions: make(map[string]Session),
+		mutex:    sync.Mutex{},
 	}
+
+	go m.collectGarbage()
 
 	return m
 }
@@ -49,9 +56,13 @@ func (m *SessionManager) CreateSession() (string, error) {
 		return "", err
 	}
 
+	m.mutex.Lock()
+
 	m.sessions[sessionID] = Session{
 		Data: make(map[string]interface{}),
 	}
+
+	m.mutex.Unlock()
 
 	return sessionID, nil
 }
@@ -63,26 +74,55 @@ var ErrSessionNotFound = errors.New("SessionID does not exists")
 // GetSessionData returns data related to session if sessionID is
 // found, errors otherwise
 func (m *SessionManager) GetSessionData(sessionID string) (map[string]interface{}, error) {
+	m.mutex.Lock()
+
 	session, ok := m.sessions[sessionID]
 	if !ok {
 		return nil, ErrSessionNotFound
 	}
+
+	m.mutex.Unlock()
+
 	return session.Data, nil
 }
 
 // UpdateSessionData overwrites the old session data with the new one
 func (m *SessionManager) UpdateSessionData(sessionID string, data map[string]interface{}) error {
+	m.mutex.Lock()
+
 	_, ok := m.sessions[sessionID]
 	if !ok {
 		return ErrSessionNotFound
 	}
 
-	// Hint: you should renew expiry of the session here
 	m.sessions[sessionID] = Session{
-		Data: data,
+		Data:      data,
+		UpdatedAt: time.Now(),
 	}
 
+	m.mutex.Unlock()
+
 	return nil
+}
+
+func (m *SessionManager) collectGarbage() {
+	for {
+		m.mutex.Lock()
+
+		var toDelete []string
+		for id, session := range m.sessions {
+			if time.Now().Sub(session.UpdatedAt) >= 5*time.Second {
+				toDelete = append(toDelete, id)
+			}
+		}
+		for _, id := range toDelete {
+			delete(m.sessions, id)
+		}
+
+		m.mutex.Unlock()
+
+		<-time.Tick(5 * time.Second)
+	}
 }
 
 func main() {
